8808b61d94d870cd70cbcbedc528306cd1b6f2b5
macie <maciejczulak@o2.pl>
2021-10-16 15:14:21 +0200

    Add world controllers with read and post

---
+++ src/main/java/pl/maciejczulak/aroundtheworld/world/controller/CountryController.java
@@ -0,0 +1,48 @@
+package pl.maciejczulak.aroundtheworld.world.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.maciejczulak.aroundtheworld.world.model.Country;
+import pl.maciejczulak.aroundtheworld.world.repository.CountryRepo;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/world/country")
+public class CountryController {
+    private static final Logger log = LoggerFactory.getLogger(CountryController.class);
+
+    public CountryRepo countryRepo;
+    public CountryController(CountryRepo countryRepo) {
+        this.countryRepo = countryRepo;
+    }
+
+    @PostMapping
+    ResponseEntity<Country> addCountry(@RequestBody Country toAdd){
+        log.info("add country");
+        Country result = countryRepo.save(toAdd);
+        return ResponseEntity.created(URI.create("/"+result.getId())).body(result);
+    }
+
+    @GetMapping
+    ResponseEntity<List<Country>> readAllCountries(){
+        log.info("read all countries");
+        return ResponseEntity.ok(countryRepo.findAll());
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<Country> readCountryById(@PathVariable Integer id) {
+        log.info("read country by Id");
+        return countryRepo.findById(id)
+                .map(country -> ResponseEntity.ok(country))
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
---
+++ src/main/java/pl/maciejczulak/aroundtheworld/world/controller/HotelController.java
@@ -0,0 +1,49 @@
+package pl.maciejczulak.aroundtheworld.world.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.maciejczulak.aroundtheworld.world.model.Country;
+import pl.maciejczulak.aroundtheworld.world.model.Hotel;
+import pl.maciejczulak.aroundtheworld.world.repository.HotelRepo;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/world/hotel")
+public class HotelController {
+    private static final Logger log = LoggerFactory.getLogger(HotelController.class);
+
+    public HotelRepo hotelRepo;
+    public HotelController(HotelRepo hotelRepo) {
+        this.hotelRepo = hotelRepo;
+    }
+
+    @PostMapping
+    ResponseEntity<Hotel> addHotel(@RequestBody Hotel toAdd){
+        log.info("add hotel");
+        Hotel result = hotelRepo.save(toAdd);
+        return ResponseEntity.created(URI.create("/"+result.getId())).body(result);
+    }
+
+    @GetMapping
+    ResponseEntity<List<Hotel>> readAllHotels(){
+        log.info("read all hotels");
+        return ResponseEntity.ok(hotelRepo.findAll());
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<Hotel> readHotelById(@PathVariable Integer id) {
+        log.info("read hotel by Id");
+        return hotelRepo.findById(id)
+                .map(hotel -> ResponseEntity.ok(hotel))
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
